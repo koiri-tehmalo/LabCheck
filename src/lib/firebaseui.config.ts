e8481ec1feca81af8a27e828295f018840fb2465
มูฮัมหมัดคอยรี เต๊ะมาลอ <khayreeteamalo@gmail.com>
2025-08-14 05:28:46 +0000

    ตอนที่ใช้แบบ ส่งลิงก์ไปที่อีเมล" (passwordless) ผมไม่ต้องการใช้แบบนี้ ผม

--- src/lib/firebaseui.config.ts
+++ src/lib/firebaseui.config.ts
@@ -6,26 +6,26 @@ const uiConfig = {
   signInOptions: [
     {
       provider: EmailAuthProvider.PROVIDER_ID,
-      // Require the user to enter a display name when signing up.
+      // ระบุว่าต้องการใช้อีเมลและรหัสผ่านในการล็อกอิน
+      signInMethod: EmailAuthProvider.EMAIL_PASSWORD_SIGN_IN_METHOD,
+      // กำหนดให้ต้องใส่ชื่อตอนสมัครสมาชิก
       requireDisplayName: true, 
-      // Specify that we want to use email and password for sign-in.
-      signInMethod: EmailAuthProvider.EMAIL_PASSWORD_SIGN_IN_METHOD
     },
   ],
   callbacks: {
-    // This function is called when a user successfully signs in.
+    // ฟังก์ชันนี้จะถูกเรียกเมื่อผู้ใช้ล็อกอินสำเร็จ
     signInSuccessWithAuthResult: function (authResult: any) {
       if (authResult.user) {
-        // Get the user's ID token.
+        // ดึง ID token ของผู้ใช้
         authResult.user.getIdToken().then((idToken: string) => {
-          // POST the token to the backend.
+          // ส่ง token ไปยัง backend เพื่อสร้าง session cookie
           return fetch('/api/auth', {
             method: 'POST',
             body: idToken,
           });
         });
       }
-      // Return true to redirect to the sign-in success URL.
+      // คืนค่า true เพื่อ redirect ไปยังหน้าหลักหลังจากล็อกอินสำเร็จ
       return true;
     },
   },
