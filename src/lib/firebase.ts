fcc536d147a015fac3d58818dc2f871a094473b0
มูฮัมหมัดคอยรี เต๊ะมาลอ <khayreeteamalo@gmail.com>
2025-08-14 05:22:01 +0000

    สมัครได้แล้วแต่loginไม่ได้ Firebase: Error (auth/api-key-not-valid.-plea

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,26 +1,20 @@
 
-// Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp, getApp, getApps, type FirebaseOptions } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  projectId: "asset-tracker-w0bxu",
-  appId: "1:706312783282:web:1de0be7aa749ac502e50ab",
-  storageBucket: "asset-tracker-w0bxu.appspot.com",
+const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "asset-tracker-w0bxu.firebaseapp.com",
-  messagingSenderId: "706312783282"
+  projectId: "asset-tracker-w0bxu",
+  storageBucket: "asset-tracker-w0bxu.appspot.com",
+  messagingSenderId: "706312783282",
+  appId: "1:706312783282:web:1de0be7aa749ac502e50ab",
 };
 
-
-// Initialize Firebase for client-side
-// This pattern prevents initializing the app more than once
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const auth = getAuth(app);
-
 
 export { app, db, auth };
